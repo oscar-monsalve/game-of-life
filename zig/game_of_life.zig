cbd420a56d7a10f410e26a39997dc85acba63ee2
oscar monsalve <oscarmonsalvec5@gmail.com>
2025-04-17 00:22:51 -0500

    added print_grid and create_grid functions

--- zig/game_of_life.zig
+++ zig/game_of_life.zig
@@ -1,5 +1,52 @@
 const std = @import("std");
 
-pub fn hello() !void {
-    std.debug.print("Hello from module\n", .{});
+const Errors = error{
+    InvalidGridSize,
+};
+
+pub fn create_grid(allocator: std.mem.Allocator, rows: u32, cols: u32, randomize: bool) ![]u32 {
+    if (rows <= 0 or cols <= 0) {
+        return Errors.InvalidGridSize;
+    }
+
+    const grid = try allocator.alloc(u32, rows * cols);
+
+    if (randomize == false) {
+        for (grid) |*cell| {
+            cell.* = 0;
+        }
+    }
+    if (randomize == true) {
+        // Using entropy to generate a truly random seed
+        var seed: u64 = undefined;
+        std.crypto.random.bytes(std.mem.asBytes(&seed));
+
+        // Initializing the PRNG with the random seed
+        var prng = std.Random.DefaultPrng.init(seed);
+        const rng = prng.random();
+
+        for (grid) |*cell| {
+            cell.* = rng.uintLessThan(u32, 2);
+        }
+    }
+
+    return grid;
+}
+
+pub fn print_grid(grid: []u32, rows: u32, cols: u32) void {
+    const LIVE_CELL = "■";
+    const DEAD_CELL = " ";
+    const HORIZONTAL = "⎯";
+    const VERTICAL = "🭲";
+    const CORNER = "+";
+
+    for (0..rows) |row| {
+        for (0..cols) |col| {
+            const index = row * cols + col;
+            const cell = grid[index];
+            const symbol = if (cell == 1) " ■ " else " ";
+            std.debug.print("{s}", .{symbol});
+        }
+        std.debug.print("\n", .{});
+    }
 }
--- zig/main.zig
+++ zig/main.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
 const game = @import("game_of_life.zig");
 
+const ROWS: u32 = 10;
+const COLS: u32 = 10;
+const RANDOMIZE: bool = true;
+
 pub fn main() !void {
-    try game.hello();
+    const allocator = std.heap.page_allocator;
+
+    const grid = try game.create_grid(allocator, ROWS, COLS, RANDOMIZE);
+    defer allocator.free(grid);
+
+    game.print_grid(grid, ROWS, COLS);
 }
